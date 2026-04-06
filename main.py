a23aa9e0608774c8fd36fbaab71f393a1b8c95ba
Olivier Croon <oliviercroon@hotmail.com>
2026-04-06 20:51:17 +0200

    Ajout des pages HTML dynamiques liées au backend Python

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-# This is a sample Python script.
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import json
+import os
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+CORS(app)  # Autorise ton site web à communiquer avec le serveur Python
+
+DB_FILE = 'database.json'
+
+# Initialiser la base de données si elle n'existe pas
+if not os.path.exists(DB_FILE):
+    with open(DB_FILE, 'w') as f:
+        json.dump([], f)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def get_db():
+    with open(DB_FILE, 'r') as f:
+        return json.load(f)
 
 
-# Press the green button in the gutter to run the script.
+def save_db(data):
+    with open(DB_FILE, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
+# --- ROUTES ---
+
+@app.route('/api/apps', methods=['GET'])
+def get_apps():
+    """Récupère la liste de toutes les applications"""
+    return jsonify(get_db())
+
+
+@app.route('/api/apps', methods=['POST'])
+def add_app():
+    """Ajoute une nouvelle application au catalogue"""
+    new_app = request.json
+    db = get_db()
+
+    # Structure de l'application
+    app_entry = {
+        "id": len(db) + 1,
+        "name": new_app.get('name'),
+        "desc": new_app.get('desc'),
+        "price": new_app.get('price'),
+        "url": new_app.get('url')
+    }
+
+    db.append(app_entry)
+    save_db(db)
+    return jsonify(app_entry), 201
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Lance le serveur sur le port 5000
+    app.run(debug=True, port=5000)
